2a39da8563ec586e5de348f9922a3b8f93664a4f
thanhnhanyt53 <nguyennguyen87a@gmail.com>
2026-08-19 17:14:45 +0700

    Create GUIConstants.java

---
+++ src/main/java/com/advancedredeem/gui/GUIConstants.java
@@ -0,0 +1,51 @@
+package com.advancedredeem.gui;
+
+import org.bukkit.Material;
+
+public final class GuiItems {
+
+    private GuiItems() {
+    }
+
+    public static final Material BACK =
+            Material.ARROW;
+
+    public static final Material CLOSE =
+            Material.BARRIER;
+
+    public static final Material SAVE =
+            Material.LIME_WOOL;
+
+    public static final Material CANCEL =
+            Material.RED_WOOL;
+
+    public static final Material CODE =
+            Material.NAME_TAG;
+
+    public static final Material EXPIRATION =
+            Material.CLOCK;
+
+    public static final Material MAX_USES =
+            Material.CHEST;
+
+    public static final Material PLAYER_USES =
+            Material.PLAYER_HEAD;
+
+    public static final Material REWARDS =
+            Material.CHEST_MINECART;
+
+    public static final Material CONDITIONS =
+            Material.COMPARATOR;
+
+    public static final Material COMMAND =
+            Material.COMMAND_BLOCK;
+
+    public static final Material ITEM =
+            Material.DIAMOND;
+
+    public static final Material ADD =
+            Material.LIME_DYE;
+
+    public static final Material DELETE =
+            Material.RED_DYE;
+}
