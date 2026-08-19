22f5ea2e085fd023f0748a2d581e03cb17dc66c9
thanhnhanyt53 <nguyennguyen87a@gmail.com>
2026-08-19 17:15:05 +0700

    Create MenuType.java

---
+++ src/main/java/com/advancedredeem/gui/MenuType.java
@@ -0,0 +1,26 @@
+package com.advancedredeem.gui;
+
+public enum MenuType {
+
+    CREATE,
+
+    EDITOR,
+
+    SETTINGS,
+
+    REWARDS,
+
+    COMMAND_REWARDS,
+
+    ITEM_REWARDS,
+
+    CONDITIONS,
+
+    CONDITION_TYPES,
+
+    CONDITION_EDITOR,
+
+    CONFIRM_DELETE,
+
+    ANVIL_INPUT
+}
